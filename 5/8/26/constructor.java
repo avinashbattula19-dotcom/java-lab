368b6f557628ce43a9e95d1d4a77e7ce161cbd7d
avinashbattula19-dotcom <avinashbattula19@gmail.com>
2026-08-05 10:44:45 +0000

    initial.commit

---
+++ 5/8/26/constructor.java
@@ -0,0 +1,14 @@
+class Test{
+    int a,b;
+    {
+        a=10;
+        b=20;
+        System.out.println("value of a"+a);
+        System.out.println("value of b"+b);
+    }
+}
+class constructor{
+    public static void main(String args[]){
+        Test T=new Test();
+    }
+}
---
+++ 5/8/26/parameter.java
@@ -0,0 +1,16 @@
+class Test{
+    Test(int n1,int n2){
+        System.out.println("the value of a"+n1);
+        System.out.println("the value of b"+n2);
+
+    }
+}
+
+class parameter 
+{
+    public static void main(String args[]){
+    int a=10;
+    int b=20;
+    Test t1=new Test(a,b); 
+}    
+}
